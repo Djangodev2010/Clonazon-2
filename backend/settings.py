c77c33ded4b64fc325d4487549b2a5bf81d5a454
imShourya18 <shouryasrivastava456@gmail.com>
2026-05-08 17:28:34 +0530

    Updated settings.py for deployment

--- backend/settings.py
+++ backend/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-erg6h-ngltwj)1)@)(()iaw-86om8qmgvde^!s_h^6b=#0ui7s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Djangodev2010.pythonanywhere.com']
 
 
 # Application definition
